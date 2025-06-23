bf4a3050cfebb261ea061b2432ba59456aefcc34
mihary_joel <mihary@adrware.mg>
2025-06-24 01:04:05 +0300

    feat: implement credential-based authentication with NextAuth, add sign-in and sign-up validation schemas using Zod, and update dependencies

--- auth.ts
+++ auth.ts
@@ -1,8 +1,55 @@
 import NextAuth from "next-auth"
 import { PrismaAdapter } from "@auth/prisma-adapter"
 import { prisma } from "@/prisma"
- 
+import Credentials from 'next-auth/providers/credentials'
+import { SignInSchema } from './schema'
+import bcrypt from 'bcrypt'
+
 export const { handlers, signIn, signOut, auth } = NextAuth({
   adapter: PrismaAdapter(prisma),
-  providers: [],
+  session: {
+    strategy: "jwt",
+  },
+  providers: [
+    Credentials({
+      credentials: {
+        email: {},
+        password: {},
+      },
+      authorize: async (credentials) => {
+        const { email, password } = await SignInSchema.parseAsync(credentials)
+
+        const user = await prisma.user.findUnique({
+          where: {
+            email,
+          },
+        })
+
+        if (!user) {
+          throw new Error('No user found')
+        }
+
+        const isValid = bcrypt.compareSync(password, user.password!)
+
+        if (!isValid) {
+          throw new Error('Invalid password')
+        }
+
+        return {
+          id: user.id,
+          name: user.name,
+          email: user.email,
+          image: user.image,
+        }
+      },
+    }),
+  ],
+  callbacks: {
+    session({ session, token }) {
+      if (token.sub && session.user) {
+        session.user.id = token.sub
+      }
+      return session
+    },
+  },
 })
